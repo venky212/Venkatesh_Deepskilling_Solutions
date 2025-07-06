55428c7e9e6af68063dc5093defdaa8038e827e5
venky212 <162432843+venky212@users.noreply.github.com>
2025-07-06 18:56:53 +0530

    Delete Exercise_4_5_6_7_8 directory

--- Exercise_4_5_6_7_8/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void fetchBooks() {
-        System.out.println("BookRepository: Fetching books...");
-    }
-}
--- Exercise_4_5_6_7_8/src/main/java/com/library/service/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.library.service;
-
-public class NotificationService {
-    public void sendNotification() {
-        System.out.println("NotificationService: Sending notification...");
-    }
-}
