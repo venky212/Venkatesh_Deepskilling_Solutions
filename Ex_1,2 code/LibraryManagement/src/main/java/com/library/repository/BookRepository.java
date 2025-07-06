c95d1db7b077a741d120fe9fb053fb32eae82703
venky212 <162432843+venky212@users.noreply.github.com>
2025-07-06 18:56:37 +0530

    Delete Ex_1,2 code/LibraryManagement directory

--- Ex_1,2 code/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void displayRepo() {
-        System.out.println("BookRepository: Fetching data from database...");
-    }
-}
